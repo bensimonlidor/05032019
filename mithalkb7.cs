d9a6f30d0a466d236deb1e794c09f41757bbde40
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-03-05 20:13:56 +0200

    Create mithalkb7.cs

---
+++ mithalkb7.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace div7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		
+			for (int i = 7; i <= 100; i=i+7)
+			{
+					Console.WriteLine(i);
+			}
+					
+				
+			}
+
+		}
+	}
